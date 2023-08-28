c0cab0607c4d7dab399fed43c3a8c6d0c4fbf93e
PracticeEveryday <developerforus@gmail.com>
2023-08-28 18:06:38 +0900

    AppConfig Spring 컨테이너 활용

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,16 +4,22 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+
         // 직접 main method에서 생성하지 않고
         // MemberService memberService = new MemberServiceImpl();
 
         // 이제는 appConfig에서 memberService를 꺼내야 한다!
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
 
+        // spring 컨테이너에 등록한다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
 
         // 1L -> Long Type의 1이라는 뜻 just 1은 오류임
